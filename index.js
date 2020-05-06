38b53790d3a7857c95bd539f1e431881ddf3f496
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-06 23:03:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
 
-  if (right < 360) {
-    dodger.style.right = `${right + 1}px`
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`
   }
 }
