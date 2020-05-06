08092fbb163b0f9fc6842a3e1eebb42a96741862
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-06 22:31:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var dodger = document.getElementById('dodger');
 dodger.style.backgroundColor = '#FF69B4';
 
 dodger.style.bottom = '0px';
+
+document.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
