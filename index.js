30b76baeccc46bf60fb02b590a81c153495c3e23
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-06 23:07:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
 
-
+// since left side of div is 180, total should be 360
   if (left < 360) {
     dodger.style.left = `${left + 1}px`
   }
