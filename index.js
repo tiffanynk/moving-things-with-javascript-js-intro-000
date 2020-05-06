ed1184e53f86bb7b25c5533c4a2de5e7a37003aa
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-06 22:59:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ document.addEventListener('keydown', function(e) {
 
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
+  var left = parseInt(leftNumbers)
 
-  if (left > 0) {
+  if (left > 360) {
     dodger.style.left = `${left - 1}px`
   }
 }
