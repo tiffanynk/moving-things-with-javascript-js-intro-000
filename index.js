495ee7bd8eb7a05ab628d618ae1ce77100ed7fec
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-06 22:40:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ dodger.style.backgroundColor = '#FF69B4';
 
 dodger.style.bottom = '0px';
 
-// document.addEventListener('keydown', function(e) {
-//   if (e.which === 37) {
-//     var leftNumbers = dodger.style.left.replace('px', '')
-//     var left = parseInt(leftNumbers, 10)
-//
-//     dodger.style.left = `${left - 1}px`
-//   }
-// });
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+   moveDodgerLeft()
+  }
+});
 
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
